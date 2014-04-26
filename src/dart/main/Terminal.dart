1958a5432fc32e26ac8c749101e2eaf53e4684d1
Jan Bernitt <jan.bernitt@gmx.de>
2014-04-26 21:19:32 +0200

    UP rename

--- src/dart/main/Terminal.dart
+++
@@ -1,5 +0,0 @@
-part of lingukit;
-
-abstract class Terminal {
-  
-}
--- src/dart/main/lingukit.dart
+++ src/dart/main/lingukit.dart
@@ -6,7 +6,7 @@ import 'dart:convert' as Encoding;
 part 'grammar.dart';
 part 'terminal.dart';
 part 'occur.dart';
-part 'ParseTree.dart';
+part 'parsetree.dart';
 part 'utf8.dart';
 part 'pattern.dart';
 
