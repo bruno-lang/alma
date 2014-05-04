ba71e6b2c97becbf0da4a1a001dca6650b1657c7
Jan Bernitt <jan.bernitt@gmx.de>
2014-05-04 17:37:59 +0200

    UP dart real unit test

--- src/dart/test/test_parser.dart
+++ src/dart/test/test_parser.dart
@@ -1,16 +1,43 @@
 import 'package:unittest/unittest.dart';
 import 'package:lingukit/lingukit.dart';
-import 'dart:io';
 
 main() {
-  print(GRAMMAR);
-  //Parser.parse(UTF8.bytes('foo'), name_);
-  Parsed bruno = Parsed.from('/home/jan/proj/bruno/lingukit/etc/bruno.grammar', 'grammar', GRAMMAR);
-  Parsed lingukit = Parsed.from('/home/jan/proj/bruno/lingukit/etc/lingukit.grammar', 'grammar', GRAMMAR);
-  Parsed json = Parsed.from('/home/jan/proj/bruno/lingukit/etc/json.grammar', 'grammar', GRAMMAR);
-  new Printer(stdout).printTree(Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.json', 'json', grammarFrom(json)));
-  Parsed xml = Parsed.from('/home/jan/proj/bruno/lingukit/etc/xml.grammar', 'grammar', GRAMMAR);
-  new Printer(stdout).printTree(Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.xml', 'document', grammarFrom(xml)));
+  test('bruno grammar parses', () => 
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/bruno.grammar', 'grammar', GRAMMAR), returnsNormally));
+  
+  test('bruno module parses', () {
+    Parsed bruno = Parsed.from('/home/jan/proj/bruno/lingukit/etc/bruno.grammar', 'grammar', GRAMMAR);
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.mod', 'module', grammarFrom(bruno)), returnsNormally);
+  });
+  
+  test('bruno namespace parses', () {
+    Parsed bruno = Parsed.from('/home/jan/proj/bruno/lingukit/etc/bruno.grammar', 'grammar', GRAMMAR);
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.ns', 'namespace', grammarFrom(bruno)), returnsNormally);
+  });
+  
+  test('lingukit grammar parses', () => 
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/lingukit.grammar', 'grammar', GRAMMAR), returnsNormally));
+  
+  test('lingukit via parsed lingukit grammar parses itself', () {  
+    Parsed lingukit = Parsed.from('/home/jan/proj/bruno/lingukit/etc/lingukit.grammar', 'grammar', GRAMMAR);
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/lingukit.grammar', 'grammar', grammarFrom(lingukit)), returnsNormally); 
+   });
+  
+  test('xml grammar parses', () => 
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/xml.grammar', 'grammar', GRAMMAR), returnsNormally));
+  
+  test('xml example parses', () {
+    Parsed xml = Parsed.from('/home/jan/proj/bruno/lingukit/etc/xml.grammar', 'grammar', GRAMMAR);
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.xml', 'document', grammarFrom(xml)), returnsNormally);
+  });
+  
+  test('json grammar parses', () => 
+      expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/json.grammar', 'grammar', GRAMMAR), returnsNormally)); 
+  
+  test('json example parses', () {
+    Parsed json = Parsed.from('/home/jan/proj/bruno/lingukit/etc/json.grammar', 'grammar', GRAMMAR);
+    expect(() => Parsed.from('/home/jan/proj/bruno/lingukit/etc/example.json', 'json', grammarFrom(json)), returnsNormally);
+  });
 }
 
 Grammar grammarFrom(Parsed p) {
