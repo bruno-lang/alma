2bdddc06923f1c9cdcf25c27195e5f879c163478
Jan Bernitt <jaanbernitt@gmail.com>
2017-11-15 21:14:12 +0100

    removed lex (own project)

--- java/test/alma/lang/TestSuite.java
+++ java/test/alma/lang/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith ( Suite.class )
-@SuiteClasses ( { TestProgram.class, TestParser.class, TestLex.class } )
+@SuiteClasses ( { TestProgram.class, TestParser.class } )
 public class TestSuite {
 
 }
